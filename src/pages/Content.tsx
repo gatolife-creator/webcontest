23cdce367b89cb256de177580782cbd10b98e283
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-09-18 20:30:52 +0900

    記事の追加。

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { C1S1 } from "../sentences/chapter1/C1S1";
 
 export const Content = () => {
   const location = useLocation();
   const search = location.search;
   const query = new URLSearchParams(search);
   return query.get("chapter") === "1" ? (
-    <>chapter1</>
+    query.get("section") === "1" ? (
+      <C1S1 />
+    ) : (
+      <></>
+    )
   ) : query.get("chapter") === "2" ? (
     <>chapter2</>
   ) : query.get("chapter") === "3" ? (
