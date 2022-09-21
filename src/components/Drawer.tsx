e37a9a2e8cea690824b854b76d66ad12f3f65bfa
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-09-21 16:48:39 +0900

    吹き出しとサイドバーの背景色を変更。

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -12,7 +12,7 @@ export const Drawer = (props: { children: React.ReactNode }) => {
       </div>
       <div className="drawer-side">
         <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
-        <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
+        <ul className="menu p-4 overflow-y-auto w-80 bg-base-200">
           {/* <!-- Sidebar content here --> */}
           <li>
             <Link to="/">ホーム</Link>
