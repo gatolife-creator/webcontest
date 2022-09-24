33def5adf5950f04a37e35107cbaaeb3a47e7666
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-09-24 22:47:11 +0900

    コンポーネントを追加・修正した。

--- src/components/MiniSectionTitle.tsx
+++ src/components/MiniSectionTitle.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const MiniSectionTitle = (props: {children: React.ReactNode}) => {
-    return <h2 className="text-2xl font-bold mt-32 mb-32 p-6 border-black border-l-2 border-b-2 border-dashed">{props.children}</h2>;
+    return <h2 className="text-2xl font-bold mt-32 mb-32 p-6 border-l-primary border-l-8">{props.children}</h2>;
 }
