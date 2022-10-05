f974189976cce7954de942cb0894cd9df64efd3b
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-10-05 20:02:50 +0900

    公開鍵暗号方式についての説明を追加した。

--- src/sentences/chapter1/C1S1.tsx
+++ src/sentences/chapter1/C1S1.tsx
@@ -180,7 +180,7 @@ export const C1S1 = () => {
         </Balloon>
 
         <Balloon direction="right">
-          でも、他のコンピューターは通信できているようですね。
+          あっ、でも他のコンピューターは通信できてる！
         </Balloon>
 
         <Balloon direction="left">
@@ -238,12 +238,46 @@ export const C1S1 = () => {
         </Balloon>
 
         <MiniSectionTitle>公開鍵暗号方式</MiniSectionTitle>
+
         <p>
-          公開鍵で暗号化、秘密鍵で復号する暗号方式です。
+          <Important>公開鍵暗号方式</Important>は、
+          <Important>暗号技術</Important>の一種です。
           <br />
-          公開鍵は、秘密鍵をもとに複数作成することが可能です。
+          ※
+          暗号技術・・・PDF等のデータを他人に盗み見られることなく、他の人と安全にデータをやり取りする技術。
+          <br />
+          <br />
+          用語がやや難しく聞こえるので、ここで少し勉強していきましょう。
+          <br />
+          まずは、<Important>公開鍵</Important>。 公開鍵とは、
+          <Important>データを暗号化するときに使われる</Important>鍵です。
+          その名の通り、誰でも使えるように外部に
+          <Important>公開</Important>されています。
+          <Important>秘密鍵</Important>から作られています。
+          <br />
+          <br />
+          次に<Important>秘密鍵</Important>。<br />
+          これは、公開鍵で暗号化したデータを
+          <Important>復号するための鍵</Important>です。
+          そして、この鍵を所持するのは鍵を作成した人のみ。
+          <Important>他の人に公開することはありません</Important>。
         </p>
-        
+        <Balloon direction="right">
+          公開鍵暗号方式…。なんだか難しいそうな響きですね。
+        </Balloon>
+        <Balloon direction="left">
+          安心しろ。決して仕組みは難しくない。<strong>秘密鍵</strong>
+          で暗号化し、<strong>公開鍵</strong>で復号する仕組みだ。
+        </Balloon>
+        <Balloon direction="left">
+          ナカモト君、下の図を見てみろ。これは、公開鍵暗号方式の仕組みを簡単にあらわしたものだ。
+        </Balloon>
+        <Balloon direction="right">
+          まずは受信者が<strong>秘密鍵</strong>と<strong>公開鍵</strong>
+          を作るんですね。
+        </Balloon>
+
+        <Image src={`${process.env.PUBLIC_URL}/imgs/スライド3.jpeg`} />
         <p>
           参考文献：https://udemy.benesse.co.jp/development/blockchain/public-key-cryptography.html
         </p>
@@ -268,7 +302,7 @@ export const C1S1 = () => {
 
         <MiniSectionTitle>まとめ</MiniSectionTitle>
         <Balloon direction="right">
-          これらの技術がブロックチェーンの土台だ。今後の説明で頻出だから、よく覚えておくように。
+          これらの技術がブロックチェーンの土台だ。かなり難しかっただろう。よく頑張った。次のページではブロックチェーンの長所や使い道について説明するぞ。
         </Balloon>
 
         <Pager
