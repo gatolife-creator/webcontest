778f7928fda3cec180c2f9f5e82ffdeee3c4e38f
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-10-21 23:09:41 +0900

    コンテンツの指定を楽にできるようにした。

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -7,6 +7,7 @@ import { C1S0 } from "../sentences/chapter1/C1S0";
 import { C1S1 } from "../sentences/chapter1/C1S1";
 import { C1S2 } from "../sentences/chapter1/C1S2";
 import { C1S3 } from "../sentences/chapter1/C1S3";
+import { C1S4 } from "../sentences/chapter1/C1S4";
 
 import { C2S0 } from "../sentences/chapter2/C2S0";
 import { C2S1 } from "../sentences/chapter2/C2S1";
@@ -16,6 +17,7 @@ import { C2S3 } from "../sentences/chapter2/C2S3";
 import { C3S0 } from "../sentences/chapter3/C3S0";
 import { C3S1 } from "../sentences/chapter3/C3S1";
 import { C3S2 } from "../sentences/chapter3/C3S2";
+import { C3STemp } from "../sentences/chapter3/C3Temp";
 
 export const Content = () => {
   const location = useLocation();
@@ -24,45 +26,14 @@ export const Content = () => {
   const chapter = Number(query.get("chapter"));
   const section = Number(query.get("section"));
 
+  const pages: React.ReactElement[][] = [
+    [],
+    [<C1S0 />, <C1S1 />, <C1S2 />, <C1S3 />, <C1S4 />],
+    [<C2S0 />, <C2S1 />, <C2S2 />, <C2S3 />],
+    [<C3S0 />, <C3S1 />, <C3S2 />, <C3STemp />],
+  ];
+
   return (
-    <Drawer>
-      {chapter === 1 ? (
-        section === 0 ? (
-          <C1S0 />
-        ) : section === 1 ? (
-          <C1S1 />
-        ) : section === 2 ? (
-          <C1S2 />
-        ) : section === 3 ? (
-          <C1S3></C1S3>
-        ) : (
-          <></>
-        )
-      ) : chapter === 2 ? (
-        section === 0 ? (
-          <C2S0 />
-        ) : section === 1 ? (
-          <C2S1 />
-        ) : section === 2 ? (
-          <C2S2 />
-        ) : section === 3 ? (
-          <C2S3></C2S3>
-        ) : (
-          <></>
-        )
-      ) : chapter === 3 ? (
-        section === 0 ? (
-          <C3S0 />
-        ) : section === 1 ? (
-          <C3S1 />
-        ) : section === 2 ? (
-          <C3S2 />
-        ) : (
-          <></>
-        )
-      ) : (
-        <></>
-      )}
-    </Drawer>
+    <Drawer>{pages[chapter][section] ? pages[chapter][section] : <></>}</Drawer>
   );
 };
