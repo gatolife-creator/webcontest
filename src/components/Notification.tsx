4e8aa236c629796624c11fc49273c228140e89bf
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-10-29 17:31:06 +0900

    Notificationコンポーネントを実装。

---
+++ src/components/Notification.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+export const Notification = (props: { text: string; time: number }) => {
+  const [isWithinTime, setIsWithinTime] = useState(true);
+  const { text, time } = props;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsWithinTime(false);
+    }, time);
+  });
+
+  return isWithinTime ? (
+    <div className="fixed top-1/2 left-1/2 z-50 mx-auto translate-x-[-50%] translate-y-[-50%] rounded-xl bg-slate-800 p-10 text-xl text-white opacity-80">
+      {text}
+    </div>
+  ) : (
+    <></>
+  );
+};
