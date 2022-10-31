403f915b19c883a3f993a707fce7da1fbc321d85
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-10-31 22:29:33 +0900

    導入を修正。

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,18 +5,20 @@ import { Drawer } from "../components/Drawer";
 import { Separator } from "../components/Separator";
 import { Pager } from "../components/Pager";
 import { SectionTitle } from "../components/SectionTitle";
+import { Balloon } from "../components/Balloon";
+import { Sum } from "../components/Sum";
 
 export const Home = () => {
-  document.title =  "ブロックチェーン入門";
+  document.title = "ブロックチェーン入門";
   return (
     <>
       <Drawer>
-        <div className="table w-full h-full bg-primary">
+        <div className="table h-full w-full bg-primary">
           <div className="table-cell align-middle">
-            <h1 className="text-4xl font-bold text-center drop-shadow-xl animate-tracking-in-expand md:text-6xl lg:text-8xl">
+            <h1 className="animate-tracking-in-expand text-center text-4xl font-bold drop-shadow-xl md:text-6xl lg:text-8xl">
               ブロックチェーン入門
             </h1>
-            <p className="text-md font-bold text-center md:text-3xl lg:text-3xl">
+            <p className="text-md text-center font-bold md:text-3xl lg:text-3xl">
               〜ノード先生、ブロックチェーンってなんですか？〜
             </p>
           </div>
@@ -56,7 +58,7 @@ export const Home = () => {
               alt=""
               className="mask mask-circle mr-10 lg:float-left"
             />
-            <h2 className="text-2xl font-bold mt-10">ノード先生</h2>
+            <h2 className="mt-10 text-2xl font-bold">ノード先生</h2>
             <p>
               天才猫学者。
               <br />
@@ -71,7 +73,7 @@ export const Home = () => {
               alt=""
               className="mask mask-circle mr-10 lg:float-left"
             />
-            <h2 className="text-2xl font-bold mt-10">ナカモト君</h2>
+            <h2 className="mt-10 text-2xl font-bold">ナカモト君</h2>
             <p>
               ノード先生の研究室でブロックチェーンの研究をすることになった新入生。
               <br />
@@ -88,7 +90,7 @@ export const Home = () => {
               alt=""
               className="mask mask-circle mr-10 lg:float-left"
             />
-            <h2 className="text-2xl font-bold mt-10">イザワ君</h2>
+            <h2 className="mt-10 text-2xl font-bold">イザワ君</h2>
             <p>
               ノード先生の助手。
               <br />
@@ -96,6 +98,35 @@ export const Home = () => {
             </p>
           </div>
           <div className="clear-left"></div>
+          <Separator />
+          <Balloon direction="right">
+            ノード先生、今日からよろしくお願いします！
+          </Balloon>
+          <Balloon direction="left">
+            よろしく、ナカモト君。こいつは私の助手、イザワ君だ。
+          </Balloon>
+          <Balloon direction="left">
+            イザワです。これからよろしくお願いします。
+          </Balloon>
+          <Balloon direction="left">
+            さて、今日から君は私の元でブロックチェーンの講義を受けることとなったが、君はブロックチェーンについてどのくらい知っているか？
+          </Balloon>
+          <Balloon direction="right">ぶ、ブロックチェーン？</Balloon>
+          <Balloon direction="left">
+            君もしかして、そんなことも知らずに来たのか？！仕方がない、君のために集中講義を行う。これからは以下の手順に沿って学習していくぞ。
+          </Balloon>
+          <Sum>
+            <li>ノード先生との会話を通してブロックチェーンについて学ぶ。</li>
+            <li>各セクションごとに確認テストを行い、理解度を確認する。</li>
+            <li>全て学習し終わったら、総合問題を解く。</li>
+          </Sum>
+
+          <Balloon direction="right">大変そうですね...。</Balloon>
+          <Balloon direction="left">
+            なぁに、心配するな。ちょっとした基礎知識を確認するだけだ。それに、全てを履修したら認定証をあげよう。
+          </Balloon>
+          <Balloon direction="right">...! 僕、頑張ります！</Balloon>
+          <Balloon direction="left">ちょろいな...。</Balloon>
 
           <Pager
             text="ブロックチェーン"
