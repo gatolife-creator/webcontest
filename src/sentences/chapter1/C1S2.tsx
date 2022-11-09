30178eae44e1b26c8b2a3a62dd4204e17727d646
gatolife-creater <gatolifechannel@gmail.com>
2022-11-09 13:14:53 +0900

    無駄なコードを省いた。

--- src/sentences/chapter1/C1S2.tsx
+++ src/sentences/chapter1/C1S2.tsx
@@ -4,23 +4,9 @@ import { Main } from "../../components/Main";
 import { Pager } from "../../components/Pager";
 import { MiniSectionTitle } from "../../components/MiniSectionTitle";
 
-import { Quiz } from "../../components/QuizGame";
 import { Important } from "../../components/Important";
 
 export const C1S2 = () => {
-  const quizzes: Quiz[] = [
-    {
-      question: "改ざん検知や認証システムに活用されるものは？",
-      options: ["バッシュ関数", "ラッシュ関数", "ハッシュ関数", "ダッシュ関数"],
-      answer: "ハッシュ関数",
-    },
-    {
-      question: "P2Pの利点は？",
-      options: ["システムダウンしにくい", "ハッキングされづらい"],
-      answer: "システムダウンしにくい",
-    },
-  ];
-
   return (
     <Main>
       <MiniSectionTitle>ブロックチェーンの歴史</MiniSectionTitle>
