a6f09aa755461634388ac16106a86386488f49e3
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-11-09 20:47:40 +0900

    バグを修正。

--- src/sentences/chapter1/C1S2.tsx
+++ src/sentences/chapter1/C1S2.tsx
@@ -5,6 +5,7 @@ import { Pager } from "../../components/Pager";
 import { MiniSectionTitle } from "../../components/MiniSectionTitle";
 
 import { Important } from "../../components/Important";
+import { SubSectionTitle } from "../../components/SubSectionTitle";
 
 export const C1S2 = () => {
   return (
@@ -26,23 +27,44 @@ export const C1S2 = () => {
         ことを証明する技術だ。
       </Balloon>
       <Balloon direction="right">すごく便利じゃないですか！</Balloon>
-      <Balloon direction="left">あぁ、しかし残念なことに、ビットコインが誕生する4年まえに特許が切れてしまったのだ。</Balloon>
+      <Balloon direction="left">
+        あぁ、しかし残念なことに、ビットコインが誕生する4年まえに特許が切れてしまったのだ。
+      </Balloon>
       <p>："How to Time-Stamp a Digital Document"</p>
 
       <SubSectionTitle>ビットコイン誕生</SubSectionTitle>
-      <Balloon direction="left">2018年、サトシ・ナカモトが、<Important>ビットコイン<Important />と呼ばれる電子決済システムについての論文を発表した。</Balloon>
-      <Balloon direction="left">そして、2019年に運用開始。遂に世界初の仮想通貨、ビットコインの誕生だ！</Balloon>
+      <Balloon direction="left">
+        2018年、サトシ・ナカモトが、<Important>ビットコイン</Important>
+        と呼ばれる電子決済システムについての論文を発表した。
+      </Balloon>
+      <Balloon direction="left">
+        そして、2019年に運用開始。遂に世界初の仮想通貨、ビットコインの誕生だ！
+      </Balloon>
       <Balloon direction="right">どんどんパフパフ～！</Balloon>
-      <Balloon direction="left">そして、そのビットコインと呼ばれる電子決済システムに使われた技術が、後に<Important>ブロックチェーン</Important>として名をはせることになる。</Balloon>
-      <Balloon direction="right">遂にブロックチェーン技術が誕生しましたね！</Balloon>
-      <Balloon direction="left">その後、ビットコインの課題を解決すべく、たくさんの<Important>アルトコイン<Important />（ビットコインをもとにした仮想通貨）が誕生した。</Balloon>
-      <Ballonn direction="left">その中でも革命的なものを次に見ていこう。</Balloon>
+      <Balloon direction="left">
+        そして、そのビットコインと呼ばれる電子決済システムに使われた技術が、後に
+        <Important>ブロックチェーン</Important>として名をはせることになる。
+      </Balloon>
+      <Balloon direction="right">
+        遂にブロックチェーン技術が誕生しましたね！
+      </Balloon>
+      <Balloon direction="left">
+        その後、ビットコインの課題を解決すべく、たくさんの
+        <Important>アルトコイン</Important>
+        （ビットコインをもとにした仮想通貨）が誕生した。
+      </Balloon>
+      <Balloon direction="left">
+        その中でも革命的なものを次に見ていこう。
+      </Balloon>
 
       <SubSectionTitle>ブロックチェーンの汎用化</SubSectionTitle>
-      <Balloon direction="right">先生、革命的なアルトコインって何ですか？早く教えてくださいよ！</Balloon>
-      <Balloon direction="left">まぁまぁ、落ち着け。実は仮想通貨よりもスケールの大きい物なのだよ。</Balloon>
+      <Balloon direction="right">
+        先生、革命的なアルトコインって何ですか？早く教えてくださいよ！
+      </Balloon>
+      <Balloon direction="left">
+        まぁまぁ、落ち着け。実は仮想通貨よりもスケールの大きい物なのだよ。
+      </Balloon>
       <Balloon direction="right">なおさら気になりますよ！</Balloon>
-      <Balloon direction="left"><Important></Important></Balloon>
       <Pager
         direction="back"
         text="第一節"
