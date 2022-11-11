c9845a261ff2eb69fbe93e246f39665a77faec56
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-11-11 21:04:39 +0900

    ブロックチェーンの歴史についての説明を微修正。

--- src/sentences/chapter1/C1S2.tsx
+++ src/sentences/chapter1/C1S2.tsx
@@ -14,9 +14,9 @@ export const C1S2 = () => {
       <MiniSectionTitle>ブロックチェーンの歴史</MiniSectionTitle>
       <SubSectionTitle>デジタル文書とタイムスタンプ</SubSectionTitle>
       <Balloon direction="left">
-        ブロックチェーンの前身となったアイデアは、1991年にまで遡る。
+        ブロックチェーンの前身となったアイデアは、1991年にまでさかのぼる。
       </Balloon>
-      <Balloon direction="right">かなり昔まで遡りますね。</Balloon>
+      <Balloon direction="right">かなり昔までさかのぼりますね。</Balloon>
       <Balloon direction="left">
         デジタル文書に<Important>タイムスタンプ</Important>
         をつけることで、文書の改ざんを防ぐというものだ。スチュアート・ヘイバー氏とスコット・ストルネッタ氏によって発表された。
@@ -24,7 +24,7 @@ export const C1S2 = () => {
       <Balloon direction="right">タイムスタンプって何ですか？</Balloon>
       <Balloon direction="left">
         簡単にいうと、データが<Important>作成された日時</Important>
-        、作成された日時から<Important>改ざんされていない</Important>
+        、それと<Important>改ざんされていない</Important>
         ことを証明する技術だ。
       </Balloon>
       <Balloon direction="right">すごく便利じゃないですか！</Balloon>
@@ -36,8 +36,8 @@ export const C1S2 = () => {
       <SubSectionTitle>ビットコイン誕生</SubSectionTitle>
       <Image src={process.env.PUBLIC_URL + "/imgs/bitcoin-logo.svg"}></Image>
       <Balloon direction="left">
-        2018年、サトシ・ナカモトが、<Important>ビットコイン</Important>（bitcoin）
-        と呼ばれる電子決済システムについての論文を発表した。
+        2018年、サトシ・ナカモトが、<Important>ビットコイン</Important>
+        （bitcoin） と呼ばれる電子決済システムについての論文を発表した。
       </Balloon>
       <Balloon direction="left">
         そして、2019年に運用開始。遂に世界初の仮想通貨、ビットコインの誕生だ！
@@ -60,7 +60,9 @@ export const C1S2 = () => {
       </Balloon>
 
       <SubSectionTitle>ブロックチェーンの汎用化</SubSectionTitle>
-      <Image src={process.env.PUBLIC_URL + "/imgs/ethereum-logo-landscape-black.png"}></Image>
+      <Image
+        src={process.env.PUBLIC_URL + "/imgs/ethereum-logo-landscape-black.png"}
+      ></Image>
       <Balloon direction="right">
         先生、革命的なアルトコインって何ですか？早く教えてくださいよ！
       </Balloon>
@@ -68,21 +70,28 @@ export const C1S2 = () => {
         まぁまぁ、落ち着け。実は仮想通貨よりもスケールの大きい物なのだよ。
       </Balloon>
       <Balloon direction="right">なおさら気になりますよ！</Balloon>
-      <Balloon direction="left">その名も<Important>イーサリアム</Important>（Ethereum）。
-        何とブロックチェーン上で<Important>プログラムの記録、実行ができてしまうのだ。</Important>
+      <Balloon direction="left">
+        その名も<Important>イーサリアム</Important>（ethereum）。
+        何とブロックチェーン上で<Important>プログラムの記録、実行</Important>
+        ができてしまうのだ。
       </Balloon>
       <Balloon direction="right">それのどこが画期的なんですか？</Balloon>
-      <Balloon direction="left">馬鹿野郎！</Balloon>
+      <Balloon direction="left">バカヤロウ！</Balloon>
       <Balloon direction="right">いてっ。</Balloon>
-      <Balloon direction="left">今までブロックチェーンに記録されていたのは<Important>取引履歴</Important>だけだった。しかしプログラムを記録できるようになったことで、仮想通貨だけでなく様々な分野に応用できるようになった。</Balloon>
-      <Balloon direction="left">ブロックチェーンの汎用化、というわけですね。</Balloon>
-      <Balloon direction="left">イーサリアムを皮切りに、金融、IoT、ゲーム、教育などへの活用に向けて開発が活発化することになったのだ。</Balloon>
-      <Pager
-        direction="back"
-      />
-      <Pager
-        direction="forward"
-      />
+      <Balloon direction="left">
+        今までブロックチェーンに記録されていたのは
+        <Important>取引履歴</Important>
+        だけだった。しかしプログラムを記録できるようになったことで、仮想通貨だけでなく
+        <Important>様々な分野に応用</Important>できるようになった。
+      </Balloon>
+      <Balloon direction="left">
+        ブロックチェーンの汎用化、というわけですね。
+      </Balloon>
+      <Balloon direction="left">
+        イーサリアムを皮切りに、金融、IoT、ゲーム、教育などへの活用に向けて開発が活発化することになったのだ。
+      </Balloon>
+      <Pager direction="back" />
+      <Pager direction="forward" />
     </Main>
   );
 };
