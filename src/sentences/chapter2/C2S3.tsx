3ecb6612d64908a3bf167d7128c70e4fb15ac268
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-11-11 23:18:40 +0900

    公開鍵暗号方式についての解説を修正した。

--- src/sentences/chapter2/C2S3.tsx
+++ src/sentences/chapter2/C2S3.tsx
@@ -12,30 +12,6 @@ export const C2S3 = () => {
   return (
     <Main>
       <MiniSectionTitle>公開鍵暗号方式</MiniSectionTitle>
-
-      <p>
-        <Important>公開鍵暗号方式</Important>は、
-        <Important>暗号技術</Important>の一種です。
-        <br />
-        ※
-        暗号技術・・・PDF等のデータを他人に盗み見られることなく、他の人と安全にデータをやり取りする技術。
-        <br />
-        <br />
-        用語がやや難しく聞こえるので、ここで少し勉強していきましょう。
-        <br />
-        まずは、<Important>公開鍵</Important>。 公開鍵とは、
-        <Important>データを暗号化するときに使われる</Important>鍵です。
-        その名の通り、誰でも使えるように外部に
-        <Important>公開</Important>されています。
-        <Important>秘密鍵</Important>から作られています。
-        <br />
-        <br />
-        次に<Important>秘密鍵</Important>。<br />
-        これは、公開鍵で暗号化したデータを
-        <Important>復号するための鍵</Important>です。
-        そして、この鍵を所持するのは鍵を作成した人のみ。
-        <Important>他の人に公開することはありません</Important>。
-      </p>
       <Balloon direction="right">
         公開鍵暗号方式…。なんだか難しいそうな響きですね。
       </Balloon>
@@ -48,20 +24,48 @@ export const C2S3 = () => {
         ナカモト君、下の図を見てみろ。これは、公開鍵暗号方式の仕組みを簡単にあらわしたものだ。
       </Balloon>
       <Balloon direction="right">
-        まずは受信者が<Important>秘密鍵</Important>と<Important>公開鍵</Important>
+        まずは受信者が<Important>秘密鍵</Important>と
+        <Important>公開鍵</Important>
         を作るんですね。
       </Balloon>
-
+      <Balloon direction="left">
+        送信者が鍵を受け取ったら、<Important>公開鍵</Important>
+        でデータを暗号化したのちに送信する。
+      </Balloon>
+      <Balloon direction="left">
+        そして、受信者側では受け取ったデータを
+        <Important>秘密鍵</Important>で復号化する。
+      </Balloon>
+      <Balloon direction="left">
+        これでめでたくデータを送信することができた。
+      </Balloon>
       <Image
         src={`${process.env.PUBLIC_URL}/imgs/public-key-crypto-system.png`}
       />
-
-      <Pager
-        direction="back"
-      />
-      <Pager
-        direction="forward"
-      />
+      <Balloon direction="right">
+        仕組みはわかったんですが、何で鍵を複数作る必要があるんですか？
+      </Balloon>
+      <Balloon direction="right">
+        ふつう、玄関の鍵を閉めるのにも開けるのにも、同じ鍵を使いますよね？
+      </Balloon>
+      <Balloon direction="left">
+        当然の疑問だな。もちろん、暗号化と復号化に同じ鍵を用いる
+        <Important>共通鍵暗号方式</Important>というものもある。
+      </Balloon>
+      <Balloon direction="left">
+        しかし、これには
+        <Important>「鍵配送問題」</Important>
+        といって、鍵の送信中に第三者に鍵を盗まれた場合、送信したデータを盗聴されてしまう恐れがあるのだ。
+      </Balloon>
+      画像
+      <Balloon direction="left">
+        一方で、公開鍵暗号方式ならば公開鍵が盗まれても、復号化に使うのは
+        <Important>秘密鍵</Important>だから問題ないというわけだ。
+      </Balloon>
+      画像
+      <Image src="#"></Image>
+      <Pager direction="back" />
+      <Pager direction="forward" />
     </Main>
   );
 };
