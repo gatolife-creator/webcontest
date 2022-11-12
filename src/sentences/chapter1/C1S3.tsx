b253416af66d0142f5aa4ed0f9d9483c617beb45
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-11-13 07:35:14 +0900

    吹き出しのデザインを微修正。

--- src/sentences/chapter1/C1S3.tsx
+++ src/sentences/chapter1/C1S3.tsx
@@ -7,7 +7,6 @@ import { Balloon } from "../../components/Balloon";
 import { Pager } from "../../components/Pager";
 import { MiniSectionTitle } from "../../components/MiniSectionTitle";
 import { SubSectionTitle } from "../../components/SubSectionTitle";
-import { Sum } from "../../components/Sum";
 import { Important } from "../../components/Important";
 
 export const C1S3 = () => {
@@ -15,53 +14,40 @@ export const C1S3 = () => {
     <>
       <Main>
         <MiniSectionTitle>ブロックチェーンの長所</MiniSectionTitle>
-
-        <SubSectionTitle>
-          たくさんのコンピューターによって管理される
-        </SubSectionTitle>
-        <Balloon direction="left">
-          多数のブロックチェーンのネットワーク参加者のコンピューターで、同じデータが保存されている。そのため、いくつかのコンピューターがダウンしても、全てのコンピュータの動作が止まらない限り、ブロックチェーンは動き続ける。
-        </Balloon>
         <Balloon direction="right">
-          あっ、これ<Important>P2P（ピア・ツー・ピア）</Important>でやったやつだ！
+          ノード先生、ここまでブロックチェーンの歴史とかを見てきましたけど、結局何がすごいんですか？
         </Balloon>
         <Balloon direction="left">
-          その通り！鋭いぞナカモト君。勉強の成果がいきているみたいだな。
+          それはズバリ、<Important>改ざんが難しい</Important>点、
+          <Important>安定して動作する</Important>点、 そして、
+          <Important>情報の透明性が高い</Important>点だ。
         </Balloon>
         <SubSectionTitle>改ざんが困難</SubSectionTitle>
         <Balloon direction="left">
-          そしてもう一つ、<Important>データの改竄が困難、という点にある。</Important>
-        </Balloon>
-
-        <SubSectionTitle>管理者となる中央機関が存在しない</SubSectionTitle>
-        <Balloon direction="left">
-          ナカモト君、みんなでデータを管理しながらも、改ざんすることは難しい。これは非常に革命的なのだ。なぜだかわかるか？
-        </Balloon>
-
-        <Balloon direction="right">う〜ん。わからないです。</Balloon>
-        <Balloon direction="left">
-          <Important>中央機関が必要なくなる</Important>
-          。そういうことですよね、ノード先生。
+          これは先ほども説明したが、ブロックチェーンは改ざんが難しいデータベースの一種であるが、
         </Balloon>
         <Balloon direction="left">
-          大正解。基本的に、情報の信憑性を保つには信用できる第三者、例えば国の機関や大企業などによる仲介が必要になる。しかしながらブロックチェーンを活用することによって、第三者の干渉無しに情報の信憑性を担保することができる。これを
-          <Important>トラストレス</Important>
-          という。これが実に革命的なのだよ。これは世の中の仕組みを変えてしまう可能性を秘めている。それをこの後説明しよう。
+          <Important>ハッシュ関数</Important>、
+          <Important>コンセンサス・アルゴリズム</Important>
+          などが、改ざんを難しくするのに一役買っている。
+        </Balloon>
+        <SubSectionTitle>安定して動作する</SubSectionTitle>
+        <Balloon direction="left">
+          不特定多数のコンピューターでデータを分散管理、通信している。
         </Balloon>
 
-        <Balloon direction="right">
-          世界を一変させてしまう技術かぁ。楽しみだなぁ。
+        <SubSectionTitle>情報の透明性が高い</SubSectionTitle>
+        <Balloon direction="left">
+          ブロックチェーンの上のデータなら、誰もがすべて見ることができる。
         </Balloon>
-        <Sum>
-          これまでのお金の取引は、政府や中央銀行、民間の銀行などの中央管理者（信頼できる管理者）によって不正がないことが保証されていました。
-          一方で、ブロックチェーンを使用することで、そのような中央管理者無しで、ブロックチェーンの参加者のみで不正がないことを確かめることができるようになったのです
-        </Sum>
-        <Pager
-          direction="back"
-        />
-        <Pager
-          direction="forward"
-        />
+
+        <Balloon direction="left">
+          このような特徴を踏まえて、<Important>所有権</Important>や
+          <Important>著作権</Important>
+          の管理などへの活用が期待されている。
+        </Balloon>
+        <Pager direction="back" />
+        <Pager direction="forward" />
       </Main>
     </>
   );
