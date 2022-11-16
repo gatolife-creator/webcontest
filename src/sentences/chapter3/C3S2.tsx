bc1878deed6d3b6696a7648357f2c74d68d0756e
gatolife-creater <gatolifechannel@gmail.com>
2022-11-17 08:22:43 +0900

    処理の流れの解説を追加。

--- src/sentences/chapter3/C3S2.tsx
+++ src/sentences/chapter3/C3S2.tsx
@@ -8,7 +8,21 @@ export const C3S2 = () => {
   return (
     <Main>
       <MiniSectionTitle>処理の流れ</MiniSectionTitle>
-      <Balloon char="node">次に、</Balloon>
+      <Balloon char="node">ブロックチェーンの基本構造を理解したところで、各コンピューターが持っているブロックチェーンの情報をどのようにして共有しているかを見ていこう。</Balloon>
+      <Balloon char="satoshi">いよいよブロックチェーンの仕組みですね！</Balloon>
+      <Balloon char="node">今回はビットコインのブロックチェーンを参考に見ていくぞ。</Balloon>
+      <Balloon char="node">まず、誰かに通貨を送る際にトランザクションを作成する。</Balloon>
+      <Balloon char="satoshi">ふむふむ。</Balloon>
+      <Balloon char="node">この時、トランザクションの送信者アドレスには、自身の作成した公開鍵を、受信者アドレスには、送る相手の公開鍵を指定する。</Balloon>
+      <Balloon char="node">そして、最後に自分の秘密鍵を使って、トランザクションに署名して、トランザクションの作成は終了だ。</Balloon>
+      <Balloon char="satoshi">ここでデジタル署名が使われているんですか？</Balloon>
+      <Balloon char="node">そうだ。勝手に他の人に自分の仮想通貨を使われたら困るだろう？</Balloon>
+      <Balloon char="satoshi">なるほど、デジタル署名は否認を防止するもの、裏を返せば自分が作成したトランザクションであることが証明できるのですね。</Balloon>
+      <Balloon char="node">トランザクションもできたし、これで他のコンピューターに送信と行きたいところだが、ここで一手間。</Balloon>
+      <Balloon char="node">作成したトランザクションが有効であるか検証する。</Balloon>
+      <Balloon char="satoshi">なんで送信する前に確認が必要なんですか？</Balloon>
+      <Balloon char="node">それは誤って、もしくは悪意があって無効なトランザクションを大量に送りつけることで、ネットワークに負荷がかかるのを防ぐためだ。</Balloon>
+      <Balloon char="node">そして、そのトランザクションを受け取った他のコンピューターは再びトランザクションが有効か確認する。</Balloon>
 
       <Pager direction="back" />
       <Pager direction="forward" />
