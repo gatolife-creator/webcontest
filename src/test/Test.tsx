79742eec3d26d3fbd6253f3b77798ccad5a0cc93
gatolife-creater <gatolifechannel@gmail.com>
2022-11-16 14:56:59 +0900

    微修正。

--- src/test/Test.tsx
+++ src/test/Test.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { Drawer } from "../components/Drawer";
 import { Main } from "../components/Main";
 
-import { Certificate } from "../components/Certificate";
 // import { BlockchainSample } from "../sample/blockchain/blockchainSample";
 
 export const Test = () => {
   console.log("rendered");
-  const [canvas, setCanvas] = useState<JSX.Element>(<Certificate />);
   return (
     <Drawer>
       <Main>
-        {canvas}
-
         {/* <BlockchainSample></BlockchainSample> */}
       </Main>
     </Drawer>
