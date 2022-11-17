679c085e13a66c792a625e72e2d48c16dc5cd6d8
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-11-18 06:15:56 +0900

    環境対策についての文章を追加。

--- src/sentences/chapter5/C5S4.tsx
+++ src/sentences/chapter5/C5S4.tsx
@@ -2,13 +2,36 @@ import React from "react";
 import { Main } from "../../components/Main";
 import { MiniSectionTitle } from "../../components/MiniSectionTitle";
 import { Pager } from "../../components/Pager";
-import { SubSectionTitle } from '../../components/SubSectionTitle';
-import { Balloon } from '../../components/Balloon';
+import { SubSectionTitle } from "../../components/SubSectionTitle";
+import { Balloon } from "../../components/Balloon";
+import { Important } from "../../components/Important";
 
 export const C5S4 = () => {
   return (
     <Main duration="long">
       <MiniSectionTitle>環境対策</MiniSectionTitle>
+      <Balloon char="satoshi">
+        ブロックチェーンで環境対策？いくら凄い技術だとしても、さすがに環境対策にも使えるとは思えませんよ。
+      </Balloon>
+      <Balloon char="node">
+        確かに、ブロックチェーンはあくまでデジタル上の技術だ。だが、ブロックチェーンの
+        <Important>透明性</Important>や<Important>即時性</Important>
+        が、<Important>流通の無駄を減らす</Important>手助けになる。
+      </Balloon>
+      <Balloon char="satoshi">どういうことですか？ </Balloon>
+      <Balloon char="node">
+        例えば食料品だ。従来の流通システムでは、食料品は生産場所からいろいろな機関を介して私たち消費者に届けられる。
+      </Balloon>
+      <Balloon char="node">
+        そのため生産者と消費者の間のどこかに食料品の不適切な扱いや輸送、保管などがあると食料品がダメになってしまう場合があるんだ。
+      </Balloon>
+      <Balloon char="satoshi">それは嫌ですね。 </Balloon>
+      <Balloon char="node">
+        だろう。そこでブロックチェーンの出番だ。ブロックチェーンを使えば食料品がどのような経路をたどってきたかが一目でわかる。これによって食料品が適切な扱いを受けているかを確かめることができるのだ。
+      </Balloon>
+      <Balloon char="satoshi">
+        なるほど。これなら食糧廃棄を減らすことができそうですね！
+      </Balloon>
 
       <MiniSectionTitle>株式会社chaintope</MiniSectionTitle>
       <SubSectionTitle>
