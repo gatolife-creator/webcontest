0b58f570c1ed79c7923a1f6fef4dd70a795e8ed2
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-11-18 20:08:47 +0900

    ゲームの説明や画像等を追加。

--- src/sentences/chapter5/C5S2.tsx
+++ src/sentences/chapter5/C5S2.tsx
@@ -5,6 +5,8 @@ import { MiniSectionTitle } from "../../components/MiniSectionTitle";
 import { Pager } from "../../components/Pager";
 import { SubSectionTitle } from "../../components/SubSectionTitle";
 import { Important } from "../../components/Important";
+import { MdOpenInNew } from "react-icons/md";
+import { Image } from "../../components/Image";
 
 export const C5S2 = () => {
   return (
@@ -34,26 +36,48 @@ export const C5S2 = () => {
         例えば、ブロックチェーン上で動作するカードゲームがあったとしよう。
       </Balloon>
       <Balloon char="node">
-        ここでもし、あるカードの発行上限を設定した場合、どんなことが起こるかな？
+        ここでもし、あるレアカードの発行上限を設定した場合、どんなことが起こるかな？
       </Balloon>
       <Balloon char="satoshi">
-        そのカードは無限にあるわけじゃないから、希少価値が上がるんじゃないですか？
+        希少価値が上がって、需要が大きくなるのでは？
       </Balloon>
-      <Balloon char="node">その通りだ。</Balloon>
+      <Balloon char="node">
+        その通りだ。そこで、レアカードを持っている人が持っていない人に売ることで、収益を得ることができるだろう。
+      </Balloon>
+      <Balloon char="satoshi">
+        そうか！ブロックチェーンだったら、チートの恐れもないしNFTの売買もできる。
+      </Balloon>
+      <Balloon char="satoshi">
+        だからブロックチェーンとゲームは相性がいいのか！
+      </Balloon>
+      <Balloon char="satoshi">僕もゲームしてお金稼ぎたいな〜。</Balloon>
 
       <Balloon char="node">
         このように、ゲームなどのコンテンツを利用し、仮想通貨やNFTなどのデジタル資産を稼ぐことを
         <Important>Play to Earn</Important>と言う。
       </Balloon>
       <Balloon char="node">
-        今回はplay top earn gameの開発に取り組んでいる、double
-        jump.tokyoを紹介しよう。
+        今回は<Important>MyCryptoHeroes</Important>を手がける、double
+        jump.tokyo株式会社を紹介しよう。
       </Balloon>
+      <Image
+        src={process.env.PUBLIC_URL + "/imgs/mch_logo_yoko_b.png"}
+        style={{ width: "100%" }}
+      />
+      <div className="text-right">
+        <small>画像提供：double jump.tokyo株式会社</small>
+      </div>
 
       <MiniSectionTitle>double jump.tokyo株式会社</MiniSectionTitle>
-      <Balloon char="node">
-        double jump.tokyo様にインタビューをお願いしました。
+      <Image
+        src={process.env.PUBLIC_URL + "/imgs/doublejumptokyo_logo.png"}
+        style={{ width: "100%" }}
+      ></Image>
+      <Balloon char="ether">
+        double jump.tokyo様にインタビューをしてきました。
       </Balloon>
+      <Balloon char="node">ありがとう、助かるよ。</Balloon>
+
       <SubSectionTitle>ゲームにブロックチェーンを活用する理由</SubSectionTitle>
       <p>
         ソーシャルゲームでは毎年数多くのサービス終了があり、サービス終了と共にユーザーの所持していたアイテムが消えてしまいます。
@@ -75,7 +99,19 @@ export const C5S2 = () => {
         ブロックチェーンを学べば学ぶほど、これから起きるイノベーションの大きさに驚くと思います。
         テクノロジーのほか、金融経済や法律などの知識があると、より面白い未来を考えることができるようになると思います。
       </p>
-      <Balloon char="node">
+
+      <p className="my-3 text-right">
+        <a
+          href="https://www.doublejump.tokyo/"
+          className="link-secondary link text-right"
+          target="blank"
+        >
+          double jump.tokyo
+          <MdOpenInNew style={{ display: "inline" }} />
+        </a>
+      </p>
+
+      <Balloon char="ether">
         double jump.tokyo様、インタビューにお答えくださりありがとうござました！
       </Balloon>
       <Balloon char="satoshi">ありがとうござました！</Balloon>
