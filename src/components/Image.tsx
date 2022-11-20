69ade36780011241d652d5bfba6aa644d2e6cbbe
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-11-21 06:15:57 +0900

    画像サイズを修正。

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -9,7 +9,7 @@ export const Image = (props: {
     <img
       src={props.src}
       alt=""
-      className={props.className ? props.className : "m-auto my-10 max-h-[90%]"}
+      className={props.className ? props.className : "m-auto my-10 !max-h-[80vh]"}
       style={props.style || { boxShadow: "5px 5px 5px 0 rgba(0, 0, 0, .3)" }}
     />
   );
