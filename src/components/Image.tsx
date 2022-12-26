aad9e61157bb41536b7d8a6eec5a17ed8c6adc12
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-12-26 11:38:45 +0900

    画像を拡大するライブラリを導入。

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,11 +1,6 @@
-import React, { useState, useRef } from "react";
-import { motion, useDomEvent } from "framer-motion";
-
-const transition = {
-  type: "spring",
-  damping: 25,
-  stiffness: 120,
-};
+import React from "react";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 export const Image = (props: {
   src: string;
@@ -13,27 +8,21 @@ export const Image = (props: {
   style?: {};
   className?: string;
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  useDomEvent(useRef(window), "scroll", () => isOpen && setIsOpen(false));
-
   return (
-    <div className={`image-container ${isOpen ? "open" : ""} my-10`}>
-      <motion.div
-        animate={{ opacity: isOpen ? 1 : 0 }}
-        transition={transition}
-        className="shade"
-        onClick={() => setIsOpen(false)}
-      />
-      <motion.img
-        src={props.src}
-        alt=""
-        className={props.className ? props.className : "mx-auto !max-h-[80vh]"}
-        style={props.style || { boxShadow: "5px 5px 5px 0 rgba(0, 0, 0, .3)" }}
-        onClick={() => setIsOpen(!isOpen)}
-        layout
-        transition={transition}
-      />
-      {props.caption && isOpen === false && (
+    <div className="relative">
+      <Zoom>
+        <img
+          src={props.src}
+          alt=""
+          className={
+            props.className ? props.className : "mx-auto my-10 !max-h-[80vh]"
+          }
+          style={
+            props.style || { boxShadow: "5px 5px 5px 0 rgba(0, 0, 0, .3)" }
+          }
+        />
+      </Zoom>
+      {props.caption && (
         <p className="absolute right-0 top-0 bg-slate-800 px-2 py-1 text-sm text-white opacity-80">
           {props.caption}
         </p>
